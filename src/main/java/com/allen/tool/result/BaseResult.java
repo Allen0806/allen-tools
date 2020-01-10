db295bd7a86bc5ffaa908d4aaf33dc7092251eb9
luoxuetong <luoxuetong@9fbank.com.cn>
2020-01-10 18:24:35 +0800

    增加类

---
+++ src/main/java/com/allen/tool/result/BaseResult.java
@@ -0,0 +1,93 @@
+package com.allen.tool.result;
+
+public class BaseResult<T> {
+
+	/**
+	 * 成功时对应的状态码
+	 */
+	public static final String STATUS_HANDLE_SUCCESS = "000000";
+
+	/**
+	 * 系统异常时对应的状态码
+	 */
+	public static final String STATUS_SYSTEM_FAILURE = "999999";
+
+	/**
+	 * 状态码
+	 */
+	private String status;
+
+	/**
+	 * 状态信息
+	 */
+	private String message;
+
+	/**
+	 * 业务对象
+	 */
+	private T result;
+
+	/**
+	 * 构造方法，默认初始化为成功状态
+	 */
+	public BaseResult() {
+		this(STATUS_HANDLE_SUCCESS, null, null);
+	}
+
+	/**
+	 * 构造方法
+	 * 
+	 * @param status  状态码
+	 * @param message 状态信息
+	 */
+	public BaseResult(String status, String message) {
+		this(status, message, null);
+	}
+
+	/**
+	 * 构造方法
+	 * 
+	 * @param status  状态码
+	 * @param message 状态信息
+	 * @param result  结果对象
+	 */
+	public BaseResult(String status, String message, T result) {
+		this.status = status;
+		this.result = result;
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public void setResult(T result) {
+		this.result = result;
+	}
+
+	/**
+	 * 判断是否处理成功
+	 * 
+	 * @return 处理结果
+	 */
+	public boolean success() {
+		return STATUS_HANDLE_SUCCESS.equals(status);
+	}
+
+}
