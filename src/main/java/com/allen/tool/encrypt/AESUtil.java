a22bdc828394a613fa8ede7a3fc6794640e818fd
luoxuetong <luoxuetong@lczq.com>
2023-03-31 17:06:10 +0800

    修改注释

--- src/main/java/com/allen/tool/encrypt/AESUtil.java
+++ src/main/java/com/allen/tool/encrypt/AESUtil.java
@@ -2,20 +2,14 @@ package com.allen.tool.encrypt;
 
 import cn.hutool.crypto.Mode;
 import cn.hutool.crypto.Padding;
-import cn.hutool.crypto.SecureUtil;
-import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.crypto.symmetric.AES;
 import com.allen.tool.exception.CustomBusinessException;
-import com.allen.tool.json.JsonUtil;
-import com.allen.tool.param.LoginParam;
 import com.allen.tool.result.ResultStatus;
 import com.allen.tool.string.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * AES加解密工具
@@ -86,84 +80,5 @@ public class AESUtil {
             LOGGER.error("AES解密失败", e);
             throw new CustomBusinessException(ResultStatus.SYSTEM_ERROR.getCode(), "AES解密失败", e);
         }
-    }
-
-    /**
-     * 加密登录信息
-     */
-    private static void encryptLoginParam() {
-        String appId = "75a33291284640079a462a0723b4bd11";
-        String md5Key = "pUctu54GzN@H4b6H";
-        String requestTime = String.valueOf(System.currentTimeMillis());
-        String requestSign = DigestUtil.md5Hex(appId + requestTime + md5Key);
-        System.out.println("request-time: " + requestTime);
-        System.out.println("request-sign: " + requestSign);
-
-
-
-        String aesKey = "WdVZ8#GYhMPsmeKQ";
-        String aesIv = "L@9xxxetu9REbQGq";
-        LoginParam loginParam = new LoginParam();
-        loginParam.setAppId(appId);
-        loginParam.setUserId("446706638821265408");
-        loginParam.setAccessToken("6e2fc16a940c49a4a515a8e01661df1e");
-        loginParam.setTenantId("lczq");
-        loginParam.setDeviceId("F351D598-525C-49A3-A50B-C1C0B05FD92D");
-        loginParam.setDeviceType("1");
-        loginParam.setIdentifyType("1");
-        loginParam.setOpStation("MI;IIP=36.110.108.162;IPORT=7004;LIP=192168070114;MAC=NA;IDFA=F351D598-525C-49A3-A50B-C1C0B05FD92D;UMPN=NA;RMPN=15210341053;ICCID=NA;OSV=iOS16.2;IMSI=NA;储宝宝+测试=4.5.6.0");
-        String loginParamStr = JsonUtil.object2Json(loginParam);
-        String encryptContent = encrypt(loginParamStr, aesKey, aesIv);
-        System.out.println("loginParam: " + encryptContent);
-
-        encryptContent = "ccztyJe+Z5IQ5VYa01KaRk/Z5lrVswXHouhUeyFH5DfuXOLO6gCCZzIcOZM8+MfBLcdL9/IjnsShJmgwTfH+OpuavS+betFI/nES6JIBw7I3BZ7+dnZK8V07fsXr83Xw7Cg4l89XVsfoW8qvAVUltphu0IaA+TlxDKmWIHuw3cX8pg3tyHwLait+ACe41R9URQikbued1QD41XX9Obw5YhTzO52fQAVB4ADkqqcfardUXql68+z43GGFayIT5gOQuM1Hm+XnHmINFYBGQwcQF9R+GxxlL6V9H8sGZ4BJRA7gextW1mFPPtmRjZlVV0ZOB4g+0uZQs1l6zA0RvxwXIcaC0FoCGy+v5+ghrKYmEcTSu+zM9r2GY2Qeo9ZrsSJ81KMgCqnkk27DqBJ+UBvr+3fjQpXDgU2oIA2+GuMfCCGMW6QStuZ9X4gCfd9djaz+uplHk7CNDiUgf8K8ruLGAYkvJUhyo0bRP/RKijFK7tn12V9pl5cblkmTMz6w74Bez2ZEZSfSHGbzBJcmf4JhZq4QFKYtfaVhbcx+3aK35S7V8+6QH/klxyppA7djlVIWfQK0R1eqlfCoiUhRh5PtpehtFN0aSQYZU3rTAS+69yPLLrM1/ytik7gjeapMY6mM1prAR5HPOTFCkNG0vl+4EzVJaFKaKcoWu7X7/D5JxdNHpXj9cbmwA9vHUGlUa5z6g0mZnxQ/rBIKtMBrYJQXlp+IwJ0uGnslu9yaU2/LVdTcHlozMKyNrTlQ/gVwj+IbbIop7Gytc5BhAecPDa0JAIuQ9GD08CbtdzlR2bzlJemyrqOV0CKA8ERZptfIho1FiC0pEqEj3rZJC4CsinIf56Ea4h5WoGebGcIyeVoZvsE=";
-        System.out.println("userInfo: " + decrypt(encryptContent, aesKey, aesIv));
-        Map<String, Object> param = new HashMap<>();
-        param.put("id", 8);
-        param.put("appName", "测试应用4");
-        param.put("aesKey", "asdfghjkl");
-        param.put("aesIv", "asdfghjkl");
-        param.put("md5Key", "asdfghjkl");
-        param.put("description", "测试应用4");
-        param.put("updatedBy", "lxt");
-        String paramStr = JsonUtil.object2Json(param);
-        System.out.println("param: " + encrypt(paramStr, aesKey, aesIv));
-
-    }
-
-    /**
-     * 加密应用的密钥
-     */
-    private static void encryptAppInfo() {
-        // 测试
-//        String aesKey = "uh2#BWySEEKldm6p";
-//        String aesIv = "WjBPGOG@8QGoTJvl";
-        // 生产
-        String aesKey = "ZqSH#kDNHrr9tW#p";
-        String aesIv = "ArK#uXmP23zT77C&";
-        String appAesKey = "EcNTd6spto5#aQIk";
-        String appAesIv = "jl5EKsvVSE!nRcmb";
-        String appMd5Key = "aV^4ivv#BkgFMIw5";
-        String appAesKeyEncrypt = encrypt(appAesKey, aesKey, aesIv);
-        String appAesIvEncrypt = encrypt(appAesIv, aesKey, aesIv);
-        String appMd5KeyEncrypt = encrypt(appMd5Key, aesKey, aesIv);
-        System.out.println("**************************");
-        System.out.println("appAesKey: " + appAesKeyEncrypt);
-        System.out.println("appAesIv: " + appAesIvEncrypt);
-        System.out.println("appMd5Key: " + appMd5KeyEncrypt);
-        System.out.println("**************************");
-        System.out.println("appAesKey: " + decrypt(appAesKeyEncrypt, aesKey, aesIv));
-        System.out.println("appAesIv: " + decrypt(appAesIvEncrypt, aesKey, aesIv));
-        System.out.println("appMd5Key: " + decrypt(appMd5KeyEncrypt, aesKey, aesIv));
-        System.out.println("**************************");
-    }
-
-    public static void main(String[] args) {
-//        SecureUtil.md5("123456lczq");
-//        System.out.println("password: " + SecureUtil.md5(SecureUtil.md5("Sn123@lczq.com") + "lczq"));
-        //Sn123@lczq.com
-//        encryptAppInfo();
-        encryptLoginParam();
-//        System.out.println(UUID.randomUUID().toString().replace("-", ""));
     }
 }
--- src/main/java/com/allen/tool/json/JsonUtil.java
+++ src/main/java/com/allen/tool/json/JsonUtil.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Json工具类，基于Jackson实现
@@ -20,97 +20,172 @@ import java.util.Map;
  */
 public class JsonUtil {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
 
-	/**
-	 * Jackson的ObjectMapper对象
-	 */
-	private static volatile ObjectMapper objectMapper;
+    /**
+     * Jackson的ObjectMapper对象
+     */
+    private static volatile ObjectMapper objectMapper;
 
-	/**
-	 * 获取ObjectMapper实例
-	 * 
-	 * @return ObjectMapper实例
-	 */
-	private static ObjectMapper getObjectMapper() {
-		if (objectMapper == null) {
-			synchronized (JsonUtil.class) {
-				if (objectMapper != null) {
-					return objectMapper;
-				}
-				objectMapper = new ObjectMapper();
-				objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-				objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-				objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-				objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
-				objectMapper.registerModule(new JavaTimeModule());
-				objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    /**
+     * 获取ObjectMapper实例
+     *
+     * @return ObjectMapper实例
+     */
+    private static ObjectMapper getObjectMapper() {
+        if (objectMapper == null) {
+            synchronized (JsonUtil.class) {
+                if (objectMapper != null) {
+                    return objectMapper;
+                }
+                objectMapper = new ObjectMapper();
+                objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+                objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+                objectMapper.registerModule(new JavaTimeModule());
+                objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 
-			}
-		}
+            }
+        }
 
-		return objectMapper;
-	}
+        return objectMapper;
+    }
 
-	/**
-	 * 将对象转换为Json字符串
-	 * 
-	 * @param object 要转换的对象
-	 * @return 转换后的Json字符串
-	 * @throws RuntimeException
-	 */
-	public static String object2Json(Object object) {
-		try {
-			return getObjectMapper().writeValueAsString(object);
-		} catch (JsonProcessingException e) {
-			LOGGER.error("将对象[{}]转为Json字符串时发生异常", object, e);
-			throw new RuntimeException("将对象转为Json字符串时发生异常", e);
-		}
-	}
+    /**
+     * 将对象转换为Json字符串
+     *
+     * @param object 要转换的对象
+     * @return 转换后的Json字符串
+     * @throws RuntimeException
+     */
+    public static String object2Json(Object object) {
+        try {
+            return getObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            LOGGER.error("将对象[{}]转为Json字符串时发生异常", object, e);
+            throw new RuntimeException("将对象转为Json字符串时发生异常", e);
+        }
+    }
 
-	/**
-	 * 将Json字符串转换为指定类型的对象
-	 * 
-	 * @param json  要转换的Json字符串
-	 * @param clazz 指定的类型
-	 * @return 指定类型的对象
-	 * @throws RuntimeException
-	 */
-	public static <T> T json2Object(String json, Class<T> clazz) {
-		try {
-			return getObjectMapper().readValue(json, clazz);
-		} catch (Exception e) {
-			LOGGER.error("将Json字符串[{}]转换为[{}]类型对象时发生异常", json, clazz.getName(), e);
-			throw new RuntimeException("将Json字符串转换为对象时发生异常", e);
-		}
-	}
+    /**
+     * 将Json字符串转换为指定类型的对象
+     *
+     * @param json  要转换的Json字符串
+     * @param clazz 指定的类型
+     * @return 指定类型的对象
+     * @throws RuntimeException
+     */
+    public static <T> T json2Object(String json, Class<T> clazz) {
+        try {
+            return getObjectMapper().readValue(json, clazz);
+        } catch (Exception e) {
+            LOGGER.error("将Json字符串[{}]转换为[{}]类型对象时发生异常", json, clazz.getName(), e);
+            throw new RuntimeException("将Json字符串转换为对象时发生异常", e);
+        }
+    }
 
-	/**
-	 * 将Json字符串转换为自定义类型或复杂类型的对象
-	 * 
-	 * @param json         要转换的Json字符串
-	 * @param valueTypeRef 自定义类型或复杂类型
-	 * @return 给定类型的对象
-	 * @throws RuntimeException
-	 */
-	public static <T> T json2Object(String json, TypeReference<T> valueTypeRef) {
-		try {
-			return getObjectMapper().readValue(json, valueTypeRef);
-		} catch (Exception e) {
-			LOGGER.error("将Json字符串[{}]转换为[{}]类型对象时发生异常", json, valueTypeRef.getType().getTypeName(), e);
-			throw new RuntimeException("将Json字符串转换为对象时发生异常", e);
-		}
-	}
+    /**
+     * 将Json字符串转换为自定义类型或复杂类型的对象
+     *
+     * @param json         要转换的Json字符串
+     * @param valueTypeRef 自定义类型或复杂类型
+     * @return 给定类型的对象
+     * @throws RuntimeException
+     */
+    public static <T> T json2Object(String json, TypeReference<T> valueTypeRef) {
+        try {
+            return getObjectMapper().readValue(json, valueTypeRef);
+        } catch (Exception e) {
+            LOGGER.error("将Json字符串[{}]转换为[{}]类型对象时发生异常", json, valueTypeRef.getType().getTypeName(), e);
+            throw new RuntimeException("将Json字符串转换为对象时发生异常", e);
+        }
+    }
 
-	/**
-	 * 将Json字符串转换为Map类型的对象
-	 * 
-	 * @param json 要转换的Json字符串
-	 * @return Map对象，key为String类型，value为Object类型
-	 * @throws RuntimeException
-	 */
-	public static Map<String, Object> json2Map(String json) {
-		return json2Object(json, new TypeReference<Map<String, Object>>() {
-		});
-	}
+    /**
+     * 将Json字符串转换为Map类型的对象
+     *
+     * @param json 要转换的Json字符串
+     * @return Map对象，key为String类型，value为Object类型
+     * @throws RuntimeException
+     */
+    public static Map<String, Object> json2Map(String json) {
+        return json2Object(json, new TypeReference<Map<String, Object>>() {
+        });
+    }
+
+    /**
+     * 将对象转换为json，给定的属性不输出
+     *
+     * @param object      给定对象
+     * @param ignoreField 需要忽略的属性
+     * @return json字符串
+     */
+    public static String object2Json(Object object, String ignoreField) {
+        List<String> ignoreFields = new ArrayList<>();
+        ignoreFields.add(ignoreField);
+        return object2Json(object, ignoreFields);
+    }
+
+    /**
+     * 将对象转换为json，给定的属性不输出
+     *
+     * @param object       给定对象
+     * @param ignoreFields 需要忽略的属性列表
+     * @return json字符串
+     */
+    public static String object2Json(Object object, List<String> ignoreFields) {
+        Object tmp = json2Object(object2Json(object), new TypeReference<Object>() {
+        });
+        remove(tmp, ignoreFields);
+        return object2Json(tmp);
+    }
+
+    /**
+     * 递归移除给定的属性
+     *
+     * @param object
+     * @param fields
+     */
+    private static void remove(Object object, List<String> fields) {
+        if (object == null) {
+            return;
+        }
+        if (object instanceof Collection) {
+            Collection collection = (Collection) object;
+            if (collection.isEmpty()) {
+                return;
+            }
+            for (Object item : (Collection) object) {
+                remove(item, fields);
+            }
+        } else if (object instanceof Map) {
+            Map<String, Object> map = (Map<String, Object>) object;
+            map = removeFromMap(map, fields);
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                Object value = entry.getValue();
+                remove(value, fields);
+            }
+        }
+    }
+
+    /**
+     * 从map对象中移除给定的key
+     *
+     * @param map  map
+     * @param keys key集合
+     * @return 移除给定key后的map
+     */
+    private static Map<String, Object> removeFromMap(Map<String, Object> map, List<String> keys) {
+        if (map == null || map.isEmpty()) {
+            return map;
+        }
+        if (keys == null || keys.isEmpty()) {
+            return map;
+        }
+        for (String key : keys) {
+            map.remove(key);
+        }
+        return map;
+    }
 }
